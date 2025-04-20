52a1a85832cc54223d5ef5bc599a388265d0ac97
mikhailpoluboyarinov <mikhailpoluboyarinov@yandex.ru>
2025-04-20 23:29:14 +0300

    footer для всех разрешений

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <Header />
+  <Footer />
 </template>
 
 <script setup>
   import Header from "./components/Header/Header.vue";
+  import Footer from "./components/Footer/Footer.vue";
 </script>
 
 <style scoped>
--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,5 +1,30 @@
 <template>
-
+  <footer class="footer">
+    <div class="footer__container">
+      <div class="footer__logo-wrapper">
+        <img class="footer__logo" src="../../assets/images/logo-footer.png" alt="Логотип PinkChicken" />
+      </div>
+      <div class="footer__privacy">
+        <p class="footer__privacy-paragraph">Политика конфиденциальности</p>
+      </div>
+      <div class="footer__email">
+        <a href="mailto:pinkchicken@adaurum.ru" class="footer__email">
+          <span class="footer__email-icon"></span>
+          pinkchicken@adaurum.ru
+        </a>
+      </div>
+      <div class="footer__languages">
+        <span class="footer__lang-icon"></span>
+        <span class="footer__lang footer__lang--active">RU</span>
+        <span class="footer__lang">EN</span>
+        <span class="footer__lang">TUR</span>
+      </div>
+      <div class="footer__socials">
+        <a href="#" class="footer__social footer__social--telegram" aria-label="Telegram"></a>
+        <a href="#" class="footer__social footer__social--whatsapp" aria-label="WhatsApp"></a>
+      </div>
+      </div>
+  </footer>
 </template>
 
 <script>
@@ -9,5 +34,5 @@ export default {
 </script>
 
 <style scoped>
-
+  @import "Footer.scss";
 </style>
