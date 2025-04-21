e8310899c8cf0b707d8532ce8ac68835aa7e0022
mikhailpoluboyarinov <mikhailpoluboyarinov@yandex.ru>
2025-04-21 16:32:42 +0300

    add helper header

--- src/components/Helper/Helper.vue
+++ src/components/Helper/Helper.vue
@@ -1,6 +1,24 @@
 <template>
     <section class="helper">
-
+      <div class="helper__header">
+        <h1 class="helper__title">
+          Личный помощник
+        </h1>
+        <div class="helper__user-wrapper">
+          <p class="helper__user-name">
+            Jim Davidson
+          </p>
+          <p class="helper__user-mail">
+            Jim Davidson@adaurum.ru
+          </p>
+        </div>
+        <button class="helper__button">
+          <img class="helper__button-icon" src="../../assets/icons/burger.svg" alt="Кнопка обратной связи">
+        </button>
+      </div>
+      <div class="helper__main">
+        <div>asd</div>
+      </div>
     </section>
 </template>
 
