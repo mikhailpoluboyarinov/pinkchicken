d50ccc422004e2679d5b67d03c5021b8f9329152
mikhailpoluboyarinov <mikhailpoluboyarinov@yandex.ru>
2025-04-21 15:57:01 +0300

    refactor folder scss for 1440

--- src/components/Folder/Folder.vue
+++ src/components/Folder/Folder.vue
@@ -1,6 +1,10 @@
 <template>
     <section class="folder">
-
+      <h1 class="folder__title">
+        Файлы
+      </h1>
+      <img class="folder__img" src="../../assets/images/folder.png">
+      <p class="folder__paragraph">Закажи у личного помощника медиаплан. Он появится в этом разделе</p>
     </section>
 </template>
 
