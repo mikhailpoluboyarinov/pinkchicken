f78f00b693784e7a9c0d42aff2c631a965288f26
mikhailpoluboyarinov <mikhailpoluboyarinov@yandex.ru>
2025-04-22 18:12:43 +0300

    add helper

--- src/components/Helper/Helper.vue
+++ src/components/Helper/Helper.vue
@@ -17,7 +17,29 @@
         </button>
       </div>
       <div class="helper__main">
-        <div>asd</div>
+        <div class="helper__chat-wrapper">
+          <div class="helper__chat-window">
+            <div class="helper__chat-preview">
+              <img class="helper__img" src="../../assets/images/chatters.png">
+              <p class="helper__paragraph">Это чат с администратором. Ты можешь с ним пообщаться по любому вопросу о нашем сервисе и узнать о ходе работы</p>
+            </div>
+            <div class="helper__chat-buttons">
+              <button class="helper__chat-button helper__chat-button--pink">Заказать медиаплан</button>
+              <button class="helper__chat-button helper__chat-button--blue">Заказать отчет</button>
+            </div>
+            <div class="helper__chat-textarea-wrapper">
+              <textarea class="helper__chat-textarea" placeholder="Введите сообщение для администратора"></textarea>
+              <div class="helper__chat-buttons-wrapper">
+                <button class="helper__button-media helper__button-media--left"></button>
+                <button class="helper__button-media helper__button-media--right"></button>
+              </div>
+              <div class="helper__button-enter-wrapper">
+                <button class="helper__button-enter"></button>
+                <span class="helper__span-enter">Enter</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
 </template>
