97a7524a2587f4de6c54cdef638847ea6cd3efb7
mikhailpoluboyarinov <mikhailpoluboyarinov@yandex.ru>
2025-04-23 02:05:20 +0300

    fix styles for mobile

--- src/components/Folder/Folder.vue
+++ src/components/Folder/Folder.vue
@@ -1,11 +1,17 @@
 <template>
-    <section class="folder">
+    <section class="folder folder__mobile--hidden">
       <h1 class="folder__title">
         Файлы
       </h1>
       <div class="folder__wrapper">
         <img class="folder__img" src="../../assets/images/folder.png">
         <p class="folder__paragraph">Закажи у личного помощника медиаплан. Он появится в этом разделе</p>
+      </div>
+    </section>
+    <section class="folder__mobile">
+      <div class="folder__buttons">
+        <button class="folder__button folder__button--active">Медиапланы</button>
+        <button class="folder__button">Отчеты</button>
       </div>
     </section>
 </template>
--- src/components/Helper/Helper.vue
+++ src/components/Helper/Helper.vue
@@ -1,16 +1,18 @@
 <template>
     <section class="helper">
       <div class="helper__header">
-        <h1 class="helper__title">
-          Личный помощник
-        </h1>
-        <div class="helper__user-wrapper">
-          <p class="helper__user-name">
-            Jim Davidson
-          </p>
-          <p class="helper__user-mail">
-            Jim Davidson@adaurum.ru
-          </p>
+        <div class="helper__assist">
+          <h1 class="helper__title">
+            Личный помощник
+          </h1>
+          <div class="helper__user-wrapper">
+            <p class="helper__user-name">
+              Jim Davidson
+            </p>
+            <p class="helper__user-mail">
+              Jim Davidson@adaurum.ru
+            </p>
+          </div>
         </div>
         <button class="helper__button">
           <img class="helper__button-icon" src="../../assets/icons/burger.svg" alt="Кнопка обратной связи">
