9cbae97992f1942bad8fb06c2cfe97e73776e4e0
mikhailpoluboyarinov <mikhailpoluboyarinov@yandex.ru>
2025-04-24 15:43:32 +0300

    Добавил функционал чату

---
+++ src/components/ChatMessage/ChatMessage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div :class="['chat-message-wrapper', isUser ? 'chat-message-wrapper--user' : 'chat-message-wrapper--admin']">
+    <img
+        v-if="!isUser"
+        class="chat-message__avatar"
+        src="../../assets/images/chatter.png"
+        alt="Admin avatar"
+    />
+    <div class="chat-message__content">
+      <div :class="['chat-message', isUser ? 'chat-message--user' : 'chat-message--admin']">
+        <p class="chat-message__text">{{ text }}</p>
+      </div>
+      <div class="chat-message__meta" :class="{ 'chat-message__meta--user': isUser }">
+        <template v-if="!isUser">
+          <span class="chat-message__name">Jim</span> {{ time }}
+        </template>
+        <template v-else>
+          {{ time }}
+        </template>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  text: String,
+  isUser: Boolean
+});
+
+const time = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+</script>
+
+<style scoped>
+  @import "ChatMessage.scss";
+</style>
--- src/components/Helper/Helper.vue
+++ src/components/Helper/Helper.vue
@@ -25,22 +25,41 @@
       <div class="helper__main">
         <div class="helper__chat-wrapper">
           <div class="helper__chat-window">
-            <div class="helper__chat-preview">
+            <div v-if="messages.length === 0" class="helper__chat-preview">
               <img class="helper__img" src="../../assets/images/chatters.png">
-              <p class="helper__paragraph">Это чат с администратором. Ты можешь с ним пообщаться по любому вопросу о нашем сервисе и узнать о ходе работы</p>
+              <p class="helper__paragraph">
+                Это чат с администратором. Ты можешь с ним пообщаться по любому вопросу о нашем сервисе и узнать о ходе работы
+              </p>
             </div>
+
+            <div v-else class="helper__chat-messages">
+              <ChatMessage
+                  v-for="(msg, index) in [...messages].reverse()"
+                  :key="index"
+                  :text="msg.text"
+                  :isUser="msg.isUser"
+              />
+              <p class="helper__chat-time">Сегодня, {{ time }}</p>
+            </div>
+
             <div class="helper__chat-buttons">
               <button class="helper__chat-button helper__chat-button--pink">Заказать медиаплан</button>
               <button class="helper__chat-button helper__chat-button--blue">Заказать отчет</button>
             </div>
             <div class="helper__chat-textarea-wrapper">
-              <textarea class="helper__chat-textarea" placeholder="Введите сообщение для администратора"  maxlength="200"></textarea>
+              <textarea
+                  class="helper__chat-textarea"
+                  placeholder="Введите сообщение для администратора"
+                  maxlength="200"
+                  v-model="message"
+                  @keydown="handleKeyDown"
+              />
               <div class="helper__chat-buttons-wrapper">
                 <button class="helper__button-media helper__button-media--left"></button>
                 <button class="helper__button-media helper__button-media--right"></button>
               </div>
               <div class="helper__button-enter-wrapper">
-                <button class="helper__button-enter"></button>
+                <button class="helper__button-enter" @click="sendMessage"></button>
                 <span class="helper__span-enter">Enter</span>
               </div>
             </div>
@@ -51,22 +70,35 @@
 </template>
 
 <script>
+import ChatMessage from '../ChatMessage/ChatMessage.vue';
 export default {
+
+  components: { ChatMessage },
 
   name: "Helper",
   data() {
     return {
       showPopup: false,
+      message: '',
+      messages: [
+
+      ],
+      time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
     };
   },
 
   methods: {
+
+    //Здесь я обрабатываю клик по кнопке "Бургер"
+
     togglePopup() {
       this.showPopup = !this.showPopup;
     },
+
     closePopup() {
       this.showPopup = false;
     },
+
     handleClickOutside(event) {
       const popupWrapper = this.$refs.popupWrapper;
       const button = this.$refs.button;
@@ -78,7 +110,39 @@ export default {
         this.showPopup = false;
       }
     },
+
+    // Здесь я работаю с сообщениями. Как в макете тут я проверяю, если сообщение не пустое,
+    // то пушу сначала сообщение админа, а потом уже свое с задержкой в 0.2. Эта ф-я для наглядности,
+    // чтобы показать, что макет работает.
+
+    sendMessage() {
+      if (this.message.trim() !== '') {
+        const userMessage = { text: this.message.trim(), isUser: true };
+        this.message = '';
+
+        this.messages.push({
+          text: 'Lörem ipsum suparad pepött då satsig och soskap metrosocial.' +
+              ' Sapongar trenåvis i hypol innan visiskap, heterovybelt.' +
+              ' Besav ditugen stenosade om exopagt.',
+          isUser: false
+        });
+
+        setTimeout(() => {
+          this.messages.push(userMessage);
+        }, 200);
+      }
+    },
+
+
+    handleKeyDown(event) {
+      if (event.key === 'Enter' && !event.shiftKey) {
+        event.preventDefault();
+        this.sendMessage();
+      }
+    }
   },
+
+  //Монтирую и размонтирую обработчик клика вне окна попапа
 
   mounted() {
     document.addEventListener("click", this.handleClickOutside);
@@ -88,7 +152,6 @@ export default {
     document.removeEventListener("click", this.handleClickOutside);
   },
 };
-
 </script>
 
 <style scoped>
